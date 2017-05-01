24282b00b06ccddca60b3bcf60aaf40bbab83f29
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-04-30 21:13:30 -0700

    next commit

---
+++ src/javascript/addition.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	exports.add = function add(a, b) {
+		return a + b;
+	};
+
+	exports.subtract = function subtract(a, b) {
+		return a - b;
+	};
+
+}());
