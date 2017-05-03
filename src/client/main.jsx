7d918b8f8fa052c4ff28b159c2f65c5c7a2a6a59
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-03 14:18:32 -0700

    moved ui modules to ui file

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,7 +3,7 @@
 (function() {
   "use strict";
 
-  var ApplicationUi = require("./application_ui.js");
+  var ApplicationUi = require("./ui/application_ui.js");
 
   React.renderComponent(new ApplicationUi(), document.body);
 }());
