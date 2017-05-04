399fd92da1a1b8d1375ea929329edba8778c469f
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-04 15:49:09 -0700

    added react testing utils

--- src/client/ui/_stock_market_table_test.jsx
+++ src/client/ui/_stock_market_table_test.jsx
@@ -1,6 +1,8 @@
 (function() {
   "use strict";
 
+  var TestUtils = React.addons.TestUtils;
+
   describe("Test", function() {
     it("does nothing", function() {
       expect(true).to.equal(true);
