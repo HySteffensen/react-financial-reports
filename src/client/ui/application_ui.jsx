595e0ff99be80b62454d2a5a0d8cda198f7d38f2
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-07 21:53:19 -0700

    added header and footer

--- src/client/ui/application_ui.jsx
+++ src/client/ui/application_ui.jsx
@@ -10,9 +10,13 @@ var StockMarketTable = require("./stock_market_table.js");
 var ApplicationUi = React.createClass({
   render: function() {
     return <div>
+      <h1>Financial Projector</h1>
       <ConfigurationPanel />
       <hr />
       <StockMarketTable />
+      <div className="footer">
+        <p>See important disclosure information in this report.</p>
+      </div>
     </div>;
   }
 });
