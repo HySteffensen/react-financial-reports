0924a08a7f7850cfd6a5e1d7e219c8168d29c2eb
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-08 11:47:37 -0700

    added ValidDollars rendering methods

--- src/client/util/_fail_fast_test.js
+++ src/client/util/_fail_fast_test.js
@@ -33,6 +33,38 @@
       }
     });
 
+    it("checks if variable is number", function() {
+      expect(unlessNumber(0)).to.not.throwException();
+      expect(unlessNumber("foo")).to.throwException(/Expected variable to be number, but was string/);
+      expect(unlessNumber({})).to.throwException(/Expected variable to be number, but was object/);
+      expect(unlessNumber([])).to.throwException(/Expected variable to be number, but was array/);
+      expect(unlessNumber(undefined)).to.throwException(/Expected variable to be number, but was undefined/);
+      expect(unlessNumber(null)).to.throwException(/Expected variable to be number, but was null/);
+      expect(unlessNumber(NaN)).to.throwException(/Expected variable to be number, but was NaN/);
+
+      expect(unlessNumber("foo", "name")).to.throwException(/Expected variable \[name\] to be number, but was string/);
+
+      function unlessNumber(variable, variableName) {
+        return function() {
+          failFast.unlessNumber(variable, variableName);
+        };
+      }
+    });
+
+    it("checks if condition is true", function() {
+      expect(unlessTrue(true)).to.not.throwException();
+      expect(unlessTrue(false)).to.throwException(/Expected condition to be true/);
+      expect(unlessTrue(false, "a message")).to.throwException(/a message/);
+      expect(unlessTrue("foo")).to.throwException(/Expected condition to be true or false/);
+      expect(unlessTrue("foo", "ignored")).to.throwException(/Expected condition to be true or false/);
+
+      function unlessTrue(variable, message) {
+        return function() {
+          failFast.unlessTrue(variable, message);
+        };
+      }
+    });
+
     it("fails when unreachable code is executed", function() {
       expect(unreachable()).to.throwException(/Unreachable code executed/);
       expect(unreachable("foo")).to.throwException(/foo/);
