18641d8188882d4441bf87add43ff5755adda75d
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-08 11:00:31 -0700

    added validDollars.toString()

--- src/client/values/_valid_dollars_test.js
+++ src/client/values/_valid_dollars_test.js
@@ -5,21 +5,20 @@
   var InvalidDollars = require("./invalid_dollars.js");
   var FailFastException = require("../util/fail_fast.js").FailFastException;
 
+  var MAX_VALID = new ValidDollars(ValidDollars.MAX_VALUE);
+  var MIN_VALID = new ValidDollars(ValidDollars.MIN_VALUE);
+
+
+  var _0 = new ValidDollars(0);
+  var _10 = new ValidDollars(10);
+  var _20 = new ValidDollars(20);
+  var _30 = new ValidDollars(30);
+  var _50 = new ValidDollars(50);
+  var _100 = new ValidDollars(100);
+  var _minus20 = new ValidDollars(-20);
+  var invalid = new InvalidDollars();
+
   describe("ValidDollars", function() {
-
-    var MAX_VALID = new ValidDollars(ValidDollars.MAX_VALUE);
-    var MIN_VALID = new ValidDollars(ValidDollars.MIN_VALUE);
-
-
-    var _0 = new ValidDollars(0);
-    var _10 = new ValidDollars(10);
-    var _20 = new ValidDollars(20);
-    var _30 = new ValidDollars(30);
-    var _50 = new ValidDollars(50);
-
-    var _minus20 = new ValidDollars(-20);
-    var invalid = new InvalidDollars();
-
     it("prevents dollars from being constructed outside of valid range", function() {
       expect(new ValidDollars(ValidDollars.MAX_VALUE + 1)).to.eql(new InvalidDollars());
       expect(new ValidDollars(ValidDollars.MIN_VALUE - 1)).to.eql(new InvalidDollars());
@@ -49,7 +48,24 @@
       expect(_30.subtractToZero(_50)).to.eql(_0);
       expect(_30.subtractToZero(invalid)).to.eql(invalid);
     });
+
+    it("flips the sign", function() {
+      expect(_0.flipSign()).to.eql(_0);
+      expect(_20.flipSign()).to.eql(_minus20);
+      expect(_minus20.flipSign()).to.eql(_20);
+    });
+
+    it("calculates percentage amount", function() {
+      expect(_100.percentage(20)).to.eql(_20);
+    });
+
+    it("determins lesser of the two values", function() {
+      expect(_20.min(_30)).to.eql(_20);
+      expect(_30.min(_20)).to.eql(_20);
+    });
   });
+
+
 
 
 
