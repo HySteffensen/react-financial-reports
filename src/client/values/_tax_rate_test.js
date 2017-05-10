e454da319a5215cedcc2a998b847236906106228
Hyrum Steffensen <hyrum.stefensen@gmail.com>
2017-05-09 20:32:03 -0700

    Implemented TaxRate

---
+++ src/client/values/_tax_rate_test.js
@@ -0,0 +1,24 @@
+/* jshint -W097 */
+
+"use strict";
+
+var TaxRate = require("./tax_rate.js");
+var ValidDollars = require("./valid_dollars.js");
+
+describe("TaxRate", function() {
+
+  var rate = new TaxRate(25);
+
+  it("determines the base tax", function() {
+    expect(rate.simpleTaxFor(new ValidDollars(1000))).to.eql(new ValidDollars(250));
+  });
+
+  it("determines the total cost incurred if you pay tax on the tax (by selling to cover)", function() {
+    expect(rate.compoundTaxFor(new ValidDollars(1000)) + "").to.equal("$333");
+  });
+
+  it("converts to string", function() {
+    expect(rate.toString()).to.equal("25%");
+  });
+
+});
